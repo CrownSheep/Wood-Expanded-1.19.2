873ee8e4a061a968a4056cc76e3685271f300a93
CrownSheep <engalnderoren@gmail.com>
2022-07-24 15:37:02 +0300

    Added an important comment

--- src/main/java/net/crownsheep/tutorialmod/item/custom/EightBallItem.java
+++ src/main/java/net/crownsheep/tutorialmod/item/custom/EightBallItem.java
@@ -1,19 +1,19 @@
 package net.crownsheep.tutorialmod.item.custom;
 
+import net.minecraft.ChatFormatting;
+import net.minecraft.client.gui.screens.Screen;
 import net.minecraft.network.chat.Component;
 import net.minecraft.util.RandomSource;
 import net.minecraft.world.InteractionHand;
 import net.minecraft.world.InteractionResultHolder;
-import net.minecraft.world.damagesource.DamageSource;
-import net.minecraft.world.entity.MoverType;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.TooltipFlag;
 import net.minecraft.world.level.Level;
-import net.minecraft.world.phys.Vec3;
+import org.jetbrains.annotations.Nullable;
 
-import java.util.Random;
-import java.util.random.RandomGenerator;
+import java.util.List;
 
 public class EightBallItem extends Item {
     public EightBallItem(Properties properties) {
@@ -23,8 +23,6 @@ public class EightBallItem extends Item {
     @Override
     public InteractionResultHolder<ItemStack> use(Level level, Player player, InteractionHand hand) {
         if (!level.isClientSide() && hand == InteractionHand.MAIN_HAND) {
-            // output a Random Number
-            // Set a Cooldown
             outputRandomNumber(player);
             player.getCooldowns().addCooldown(this, 20);
         }
@@ -33,12 +31,24 @@ public class EightBallItem extends Item {
         return super.use(level, player, hand);
     }
 
+    @Override
+    public void appendHoverText(ItemStack itemStack, @Nullable Level level, List<Component> components, TooltipFlag flag) {
+        if(Screen.hasShiftDown()) {
+            components.add(Component.literal("Right click to get a random number!").withStyle(ChatFormatting.AQUA));
+        } else {
+            components.add(Component.literal("Press SHIFT for more info").withStyle(ChatFormatting.YELLOW));
+        }
+
+
+        super.appendHoverText(itemStack, level, components, flag);
+    }
+
     private void outputRandomNumber(Player player) {
         player.sendSystemMessage(Component.literal("Your Number is " + getRandomNumber()));
     }
 
     private int getRandomNumber() {
-        return RandomSource.createNewThreadLocalInstance().nextInt(0, 11);
+        return RandomSource.createNewThreadLocalInstance().nextInt(1, 11);
     }
 }
 
